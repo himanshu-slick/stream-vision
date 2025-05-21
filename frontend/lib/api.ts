34f6fa37776e7907331f796b67d9aab6e0f9dfda
himanshu-ac <himanshu@aegiscovenant.com>
2025-05-21 13:53:29 +0530

    fix : updated api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8000';
-console.log('API_BASE_URL', process.env.API_BASE_URL);
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
+console.log('API_BASE_URL', process.env.NEXT_PUBLIC_API_BASE_URL);
 export const startHLSStream = async (
   rtspUrl: string,
   username?: string,
